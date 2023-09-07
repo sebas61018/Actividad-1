2427dd21c8a6827b36ca23b9ccaa3f1957f382f8
sebastian <sebas61018@gmail.com>
2023-09-07 12:29:56 -0500

    Primer comit Actividad 1

---
+++ actividad/actividad/src/main/java/com/Actividad1/actividad/ActividadApplication.java
@@ -0,0 +1,13 @@
+package com.Actividad1.actividad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadApplication.class, args);
+	}
+
+}
